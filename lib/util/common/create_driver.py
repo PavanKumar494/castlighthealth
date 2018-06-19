59c6842912cb83316243dca53fe07ecdc9736fa9
PavanKumar494 <shweta.cg11@gmail.com>
2018-06-19 14:58:35 +0530

    jhjyh

--- lib/util/common/create_driver.py
+++ lib/util/common/create_driver.py
@@ -1,16 +1,17 @@
-from selenium.webdriver import Chrome,Firefox,Ie
+from selenium.webdriver import Chrome, Firefox, Ie
 import pytest
+
 
 def get_browser_instance():
     browser_type = pytest.config.option.browser
     env = pytest.config.option.env
     if env == "remote":
         if browser_type == "firefox":
-            browser = Firefox("e:/geckodriver.exe")
+            browser = Firefox(executable_path="E:\Pavan\geckodriver-v0.20.1-win64\geckodriver")
         elif browser_type == "chrome":
-            browser = Chrome("e:/chromedriver.exe")
+            browser = Chrome(executable_path="E:\Pavan\chromedriver_win32\chromedriver")
     browser.maximize_window()
     browser.implicitly_wait(30)
     browser.get("https://us.castlighthealth.com")
     return browser
-        # else:
+    # else:
